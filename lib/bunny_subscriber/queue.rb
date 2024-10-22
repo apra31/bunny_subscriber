4a19a59238701af732bb5773b3e40af0ce0b12b3
apra31 <a.pra31@gmail.com>
2024-10-22 12:40:10 +0700

    Log queue.rb configuration

--- lib/bunny_subscriber/queue.rb
+++ lib/bunny_subscriber/queue.rb
@@ -45,6 +45,7 @@ module BunnySubscriber
       end
 
 
+      puts "consuming queue => QUEUE: #{consumer.subscriber_options[:queue_name]}, OPTIONS: #{options}"
       channel.queue(
         consumer.subscriber_options[:queue_name], options
       )
